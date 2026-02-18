add853f404594dced6bf53700e2ecb5472d52bfd
YoungJin <114969291+zx0725200@users.noreply.github.com>
2026-02-18 23:17:54 +0900

    캐릭터셀렉 -> 타이틀로 코드 변경

---
+++ WarLegend/Source/WarLegend/Screen/ScreenTitle.cpp
@@ -0,0 +1,35 @@
+﻿#include "ScreenTitle.h"
+
+#include "DataManager/UIManager.h"
+#include "DataManager/UIManagerImpl.h"
+#include "ETC/Define.h"
+
+void UScreenTitle::OnEnable()
+{
+	Super::OnEnable();
+}
+
+void UScreenTitle::OnDisable()
+{
+	Super::OnDisable();
+}
+
+void UScreenTitle::OnClickEvent(const FName& InChildName)
+{
+	Super::OnClickEvent(InChildName);
+	
+	if (InChildName == TEXT("Btn_Confirm"))
+	{
+		OnClickedConfirm();
+	}
+}
+
+void UScreenTitle::OnClickedConfirm()
+{
+	Hide();
+	
+	if (const auto UIMgr = GTUIGetMgrImpl(UIManager))
+	{
+		UIMgr->ShowUI(TEXT("HudPlayerState"));
+	}
+}
---
+++ WarLegend/Source/WarLegend/Screen/ScreenTitle.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Core/ScreenWidgetBase.h"
+#include "ScreenTitle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARLEGEND_API UScreenTitle : public UScreenWidgetBase
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void OnEnable() override;
+	virtual void OnDisable() override;
+	virtual void OnClickEvent(const FName& InChildName) override;
+	
+private:
+	void OnClickedConfirm();
+};
