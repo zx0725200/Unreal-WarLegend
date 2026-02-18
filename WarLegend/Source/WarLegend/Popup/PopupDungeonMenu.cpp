cb8a00ce86a0a81f70f4192f753c5b4b425c5202
YoungJin <114969291+zx0725200@users.noreply.github.com>
2026-02-18 23:16:38 +0900

    던전,인벤토리 베이스코드 생성

---
+++ WarLegend/Source/WarLegend/Popup/PopupDungeonMenu.cpp
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PopupDungeonMenu.h"
+
+void UPopupDungeonMenu::Awake()
+{
+	Super::Awake();
+}
+
+void UPopupDungeonMenu::OnEnable()
+{
+	Super::OnEnable();
+}
+
+void UPopupDungeonMenu::OnDisable()
+{
+	Super::OnDisable();
+}
+
+void UPopupDungeonMenu::OnClickEvent(const FName& InChildName)
+{
+	Super::OnClickEvent(InChildName);
+}
---
+++ WarLegend/Source/WarLegend/Popup/PopupDungeonMenu.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Core/PopupWidgetBase.h"
+#include "PopupDungeonMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARLEGEND_API UPopupDungeonMenu : public UPopupWidgetBase
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void Awake() override;
+	virtual void OnEnable() override;
+	virtual void OnDisable() override;
+	virtual void OnClickEvent(const FName& InChildName) override;
+};
