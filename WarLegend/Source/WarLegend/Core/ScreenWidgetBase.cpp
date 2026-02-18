6ad9e8373db271b4acfb58beeffc22878417f6e8
YoungJin <114969291+zx0725200@users.noreply.github.com>
2026-02-18 23:17:37 +0900

    스크린 베이스코드 추가

---
+++ WarLegend/Source/WarLegend/Core/ScreenWidgetBase.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ScreenWidgetBase.h"
+
+void UScreenWidgetBase::Awake()
+{
+	Super::Awake();
+}
+
+void UScreenWidgetBase::OnEnable()
+{
+	Super::OnEnable();
+}
+
+void UScreenWidgetBase::OnDisable()
+{
+	Super::OnDisable();
+}
---
+++ WarLegend/Source/WarLegend/Core/ScreenWidgetBase.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WLUserWidgetBase.h"
+#include "ScreenWidgetBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARLEGEND_API UScreenWidgetBase : public UWLUserWidgetBase
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void Awake() override;
+	virtual void OnEnable() override;
+	virtual void OnDisable() override;
+	
+};
