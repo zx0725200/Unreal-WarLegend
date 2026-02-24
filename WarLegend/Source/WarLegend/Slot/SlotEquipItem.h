04a8f665d7426cabbbdc08fe2313575042d09f2d
YoungJin <114969291+zx0725200@users.noreply.github.com>
2026-02-24 17:39:17 +0900

    이미지 추가
    HUD에 필요한 이미지 추가

---
+++ WarLegend/Source/WarLegend/Slot/SlotEquipItem.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Core/SlotWidgetBase.h"
+#include "SlotEquipItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARLEGEND_API USlotEquipItem : public USlotWidgetBase
+{
+	GENERATED_BODY()
+};
